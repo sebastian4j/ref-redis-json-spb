b204233c5f539241974265cf82f50531f5dbf9b4
Sebastian Avila A <sebastian4j@gmail.com>
2019-05-27 14:17:58 -0400

    test containers scope modificado

--- src/main/java/cl/sebastian/redis/string/RedisStringApplication.java
+++ src/main/java/cl/sebastian/redis/string/RedisStringApplication.java
@@ -1,16 +1,11 @@
 package cl.sebastian.redis.string;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RedisStringApplication implements CommandLineRunner {
-
-  @Autowired
-  private RedisTemplate<String, Object> redis;
 
   public static void main(String[] args) {
     SpringApplication.run(RedisStringApplication.class, args);
@@ -18,6 +13,7 @@ public class RedisStringApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
+    // :)
   }
 
 }
